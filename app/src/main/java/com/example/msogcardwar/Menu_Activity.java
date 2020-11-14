2ff9052125d09a59af78e978f64c2b5a0da44011
Omri Gilhar <omrig3@mail.afeka.ac.il>
2020-11-14 18:21:20 +0200

    Fix minor sytax issues, remove unnececarry packges
    	* Change all on click listener to a lambda expresion
    	* Suppress wearning of unchecked, for stack casting

--- app/src/main/java/com/example/msogcardwar/Menu_Activity.java
+++ app/src/main/java/com/example/msogcardwar/Menu_Activity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 public class Menu_Activity extends AppCompatActivity {
@@ -26,12 +25,7 @@ public class Menu_Activity extends AppCompatActivity {
     }
 
     private void init_views() {
-        start_game_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGameView();
-            }
-        });
+        start_game_btn.setOnClickListener(v -> openGameView());
     }
 
     private void openGameView() {
